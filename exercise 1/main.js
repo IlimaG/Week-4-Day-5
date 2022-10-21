fc88ba048d17b4ed24d792e81211818065cdaed7
IlimaG <ivanilimag@gmail.com>
2022-10-21 21:04:30 +0200

    primer ejercicio js

---
+++ exercise 1/main.js
@@ -0,0 +1,25 @@
+// 3
+
+let cheeseCalories = 200;
+
+let eggsRequired = 4;
+let caloriesPerEgg = 75;
+
+let eggCalories = eggsRequired * caloriesPerEgg;
+
+// 4
+
+console.log ( "El total de calorias de los huevos son:", eggCalories);
+
+// 5
+
+let omeletteDuFromageCalories = cheeseCalories + eggCalories;
+
+// 6
+
+console.log ("El total de calorias de la tarta es:", omeletteDuFromageCalories)
+
+// 7 
+
+console.log ("La porción de tarta tiene:", omeletteDuFromageCalories / 2)
+
